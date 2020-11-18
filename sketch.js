604cbfd0bca7b4bf9ff4ac36b0cbf8eee385b816
HarleyC99 <73440815+HarleyC99@users.noreply.github.com>
2020-11-17 19:49:23 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var bullet,wall;
+var speed,weight;
+var thickness;
+
+
+
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(223,321);
+  weight=random(30,52);
+  bullet=createSprite(50,200,50,50);
+  thickness = random(22,83);
+  wall=createSprite(1200,200,thickness,height/2);
+    bullet.velocityX = speed;
+}
+
+function draw() {
+  background(255,255,255);  
+   //hint 3 here
+   if(hasCollided(bullet, wall))
+  {
+    bullet.velocityX=0;
+    var damage=0.5 * weight * speed * speed/(thickness *thickness *thickness);
+    
+    
+   if(damage>10)
+  {
+     wall.shapeColor=color(255,0,0);
+
+  }
+
+
+
+  if(damage>10)
+  {
+     wall.shapeColor=color(0,255,0);
+
+
+  }
+
+
+  drawSprites();
+}
+ //hint 2  here
+ function hasCollided(lbullet, lwall)
+
+ { 
+      bulletRightEdge=lbullet.x +lbullet.width;
+      wallleftEdge=lwall.x;
+      if(bullet>=wallLeftEdge)
+      {
+          return true
+      }
+      return false;
+ }
